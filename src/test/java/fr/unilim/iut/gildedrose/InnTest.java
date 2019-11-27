ab1ea72c4fdab03b63c7cbc6f9808d6863100591
azf786 <49955270+azf786@users.noreply.github.com>
2019-11-27 14:50:46 +0100

    Initial commit: récupération du code legacy

---
+++ src/test/java/fr/unilim/iut/gildedrose/InnTest.java
@@ -0,0 +1,11 @@
+package fr.unilim.iut.gildedrose;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class InnTest {
+    @Test
+    public void should_write_tests() {
+    }
+}
